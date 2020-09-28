e53ff4468515c602c216b3e12d3dd989f661140c
aservmz@naver.com <aservmz@naver.com>
2020-09-29 00:02:51 +0900

    First Commit

---
+++ NextStep/src/main/java/webserver/WebServer.java
@@ -0,0 +1,35 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server started {} port.", port);
+
+            // 클라이언트가 연결될때까지 대기한다.
+            // 서버소켓은 클라이언트 요청이 발생할 때까지 대기하다가 발생하면
+            // 연결을 담당하는 소켓을 RequestHandler 에 전달
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start();
+            }
+        }
+    }
+}

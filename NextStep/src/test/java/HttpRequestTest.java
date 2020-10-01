49f7737a4745f6c6154255895fc75afc812898f5
aservmz@naver.com <aservmz@naver.com>
2020-10-01 21:52:49 +0900

    [Chapter 5] HTTP 웹 서버 리팩토링 1단계 Test 코드

---
+++ NextStep/src/test/java/HttpRequestTest.java
@@ -0,0 +1,33 @@
+import http.HttpRequest;
+import org.junit.jupiter.api.Test;
+import util.HttpRequestUtils;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpRequestTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws Exception{
+        InputStream in = new FileInputStream(new File(testDirectory+"/Http_GET.http"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals("GET", request.getMethod());
+        assertEquals("/user/create", request.getURL());
+        assertEquals("keep-alive", request.getHeaders("Connection"));
+        assertEquals("DKY", request.getRequestParams("userId"));
+    }
+
+    @Test
+    public void request_POST() throws Exception{
+        InputStream in = new FileInputStream(new File(testDirectory+"/Http_POST.http"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals("POST", request.getMethod());
+        assertEquals("/user/create", request.getURL());
+        assertEquals("keep-alive", request.getHeaders("Connection"));
+        assertEquals("DKY", request.getRequestParams("userId"));
+    }
+}
